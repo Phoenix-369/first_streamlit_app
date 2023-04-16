059680267925cb860a39aa034dc81a95de6a2d37
Phoenix-369 <130643374+Phoenix-369@users.noreply.github.com>
2023-04-17 03:23:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,25 +26,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
-
-#New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
+#New Section to display fruityvice api response 
+streamlit.header("Fruityvice Fruit Advice!")
 try:
-	fruit_choice = streamlit.text_input('What fruit would you like information about?')
-	if not fruit_choice:
-		streamlit.error("Please select a fruit to get information.")
-	else:
-		fruityvice_response = requests.get(https://fruitvice.com/api/fruit/" + fruit_choice)
-		fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-		streamlit.dataframe(fruityvice_normalized)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+ except URLError as e:
+    streamlit.error()
 
-						   
-except URLError as e:
-	streamlit.error()
-
-#don't run anything past here while we troubleshoot
-	streamlit.stop()
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 #import snowflake.connector
 
@@ -61,4 +58,3 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This will not work correctly,but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
